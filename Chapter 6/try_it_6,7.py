dd263952d2cf5e3fb45ba15a38a5d3b2479f6e83
lsj2022 <lisjames721@gmail.com>
2022-08-15 10:27:37 -0400

    Python Project

--- Chapter 6/try_it_6,7.py
+++ Chapter 6/try_it_6,7.py
@@ -64,3 +64,69 @@ favorite_place = {
 
 for name, place in favorite_place.items():
     print(f"{name}'s favorite place to travel is {place.title()}.")
+
+
+#6-10. Favorite Numbers: Modify your program from Exercise 6-2 (page 99)
+#so each person can have more than one favorite number. Then print each person’s
+#name along with their favorite numbers.
+
+favorite_number= {'Renny': [1,2], 'Thomas':[2,4], 'Lis': [3,6] ,'James': [4,5,6] ,'Sherly': [5,100,21]}
+
+for names, numbers in favorite_number.items():
+    print(f"{names}'s favorite numbers are:")
+    for number in numbers:
+        print(number)
+
+#6-11. Cities: Make a dictionary called cities. Use the names of three cities as
+#keys in your dictionary. Create a dictionary of information about each city and
+#include the country that the city is in, its approximate population, and one fact
+#about that city. The keys for each city’s dictionary should be something like
+#country, population, and fact. Print the name of each city and all of the information
+#you have stored about it.
+
+cities = {
+    'richmond': {
+        'country':'America',
+        'population': 18_000,
+        'fact': 'capital of virginia'
+    },
+    "athens": {
+        'country':'Greece',
+        'population': 200_000,
+        'fact':'capital of Greece'
+    },
+    "london":{
+       'country': 'England',
+       'population': 250_000,
+       'fact':'capital of england'
+    },
+    'Paris':{
+       'country': 'France', 'population': 150_000, 'fact':'capital of france'
+    }
+}
+for city, info in cities.items():
+    print(f"{city.title()} is in {info['country']} and has {info['population']} people. It is also the {info['fact']}.")
+
+
+
+#6-12. Extensions: We’re now working with examples that are complex enough
+#that they can be extended in any number of ways. Use one of the example programs
+#from this chapter, and extend it by adding new keys and values, changing
+#the context of the program or improving the formatting of the output.
+
+#make a dictionary of favorite super heroes and their powers and favorite movie and say everything you know about it
+
+super_heroes= {'batman': {
+    'power':'none', 'colors': 'black', 'movie': 'Batman Returns'
+},
+    'super man':{
+    'power':'laser eyes', 'colors': 'red', 'movie':'superman'
+},
+    'Thor': {
+        'power': 'thunder', 'colors': 'red', 'movie': 'Thor Ragnorak'
+    },
+}
+
+for super_hero, facts in super_heroes.items():
+    print(f"My favorite super hero is {super_hero.title()}. Their power is {facts['power']}, their color is {facts['colors']} and my favorite movie is {facts['movie']}.")
+
